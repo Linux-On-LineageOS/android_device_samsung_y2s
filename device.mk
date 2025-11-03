aa708a99f5aebba70fdb6c1351b5c27c98a55268
Archie Meng <archiemeng@protonmail.com>
2025-11-03 19:48:38 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -74,3 +74,4 @@ $(call inherit-product, device/samsung/universal9830-common/device-hubble.mk)
 
 # Inherit from the proprietary version
 $(call inherit-product-if-exists, vendor/samsung/y2s/y2s-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
